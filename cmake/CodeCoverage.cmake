cd96aac41cfb36e5eea17a8765212747c71245c8
Swanav <sswanav@gmail.com>
2020-10-15 19:24:42 +0530

    Fix Gcovr paths

--- cmake/CodeCoverage.cmake
+++ cmake/CodeCoverage.cmake
@@ -151,7 +151,7 @@ FUNCTION(SETUP_TARGET_FOR_COVERAGE_COBERTURA _targetname _testrunner _outputname
 		${_testrunner} ${ARGV3}
 
 		# Running gcovr
-		COMMAND ${GCOVR_PATH} -x -r ${CMAKE_SOURCE_DIR} -e '${CMAKE_BINARY_DIR}/googletest-*/**' -e '${CMAKE_SOURCE_DIR}/tests/' -e'${CMAKE_SOURCE_DIR}/examples/' -e'${CMAKE_SOURCE_DIR}/build/'  -o ${_outputname}.xml
+		COMMAND ${GCOVR_PATH} -x -r ${CMAKE_SOURCE_DIR} -e '${CMAKE_SOURCE_DIR}/tests/' -e'${CMAKE_SOURCE_DIR}/examples/' -e'${CMAKE_SOURCE_DIR}/build/'  -o ${_outputname}.xml
 		WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
 		COMMENT "Running gcovr to produce Cobertura code coverage report."
 	)
